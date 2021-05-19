345a3b05eeb650da22e461fa290c72806a8b4b71
codingphase <codingphase@gmail.com>
2021-05-18 22:40:21 -0400

    remove Image import

--- pages/home.js
+++ pages/home.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import MainLayout from '../components/Layouts/MainLayout'
 import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia'
 import ForYouList from '../components/UI/ForYouList/ForYouList'
--- pages/movie.js
+++ pages/movie.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import MainLayout from '../components/Layouts/MainLayout'
 import CastInfo from '../components/UI/CastInfo/CastInfo'
 import FeaturedMedia from '../components/UI/FeaturedMedia/FeaturedMedia'
