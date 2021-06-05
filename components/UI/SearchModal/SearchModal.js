e769852192d03661bf5fa8738574ba1646f717a9
codingphase <codingphase@gmail.com>
2021-06-05 00:38:24 -0400

    Work on search Modal

--- components/UI/SearchModal/SearchModal.js
+++ components/UI/SearchModal/SearchModal.js
@@ -1,16 +1,46 @@
+import { useState, useEffect } from "react";
 import { useStateContext } from "../../HBOProvider";
+import axios from 'axios';
 
 const SearchModal = (props) => {
 	const globalState = useStateContext();
+	const [popData, setPopData] = useState([]);
+	const [searchData, setSearchData] = useState([]);
+	const [showResults, setShowResults] = useState(false);
+	const [text, setText] = useState('');
 
-	const loopComp = (comp, digit) => {
-		let thumbnails = [];
-		for (let index = 1; index <= digit; index++) {
-			thumbnails.push(comp);
+	useEffect(async () => {
+		try {
+			let popData = await axios.get(
+				`https://api.themoviedb.org/3/discover/movie?primary_release_year=2021&api_key=1db7688f317e15dd2ee2933dae838634&language=en-US`,
+			);
+			setPopData(popData.data.results.filter((item,i)=>i<14))
+			
+			setShowResults(false)
+			console.log('popdata', popData.data.results)
+		} catch (error) {
+			console.log("error");
+			console.log(error);
 		}
+	}, [])
+	
 
-		return thumbnails;
-	};
+	const handleInput = async (e) => {
+		try {
+			setText(e.target.value)
+			let popData = await axios.get(
+				`https://api.themoviedb.org/3/search/multi?query=${e.target.value}&api_key=1db7688f317e15dd2ee2933dae838634&language=en-US`,
+			);
+			setSearchData(popData.data.results.filter((item, i) => item.media_type === 'tv' || item.media_type === 'movie'))
+			setShowResults(true)
+		} catch (error) {
+			setShowResults(false)
+		}
+		
+		// console.log('searchResult', popData.data.results.filter((item, i) => item.media_type === 'tv' || item.media_type === 'movie'))
+		// setSearchData()
+	}
+
 	return (
 		<div
 			className={`search-modal ${
@@ -21,7 +51,8 @@ const SearchModal = (props) => {
 					className="search-modal__input"
 					type="text"
 					placeholder="search for a title"
-					
+					value={text}
+					onChange={handleInput}
 				/>
 				<div
 					className="search-modal__close-btn"
@@ -35,15 +66,42 @@ const SearchModal = (props) => {
 			<h3 className="search-modal__title">Popular Searches</h3>
 
 			<div className="search-modal__thumbnails">
-				<div className="search-modal__thumbnail">
-						<img src="https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88997/93196/Avengers-Endgame-Final-Style-Poster-buy-original-movie-posters-at-starstills__42370.1563973516.jpg?c=2?imbypass=on" />
-						<div className="search-modal__top-layer">
-							<i className="fas fa-play" />
-						</div>
-					</div>
+					{showResults && searchData.length > 1 ? <SearchResults searchData={searchData} /> : <PopularResults popData={popData}  /> }
 			</div>
 		</div>
 	);
 };
+
+const PopularResults = (props) => {
+	return props.popData.map((item, index) => {
+		return(
+			<div className="search-modal__thumbnail" key={index}>
+				<img src={`https://image.tmdb.org/t/p/w185${item.poster_path}`} />
+				<div className="search-modal__top-layer">
+					<i className="fas fa-play" />
+				</div>
+			</div>
+		)
+	})
+}
+
+const SearchResults = (props) => {
+	console.log(props.searchData)
+	// return(<h1>test</h1>)
+	
+	return props.searchData.map((item, index) => {
+		return (
+			<div className="search-modal__thumbnail" key={index}>
+				<a href={`/${item.media_type}/${item.id}`}>
+					<img src={`https://image.tmdb.org/t/p/w185${item.poster_path}`} />
+					<div className="search-modal__top-layer">
+						<i className="fas fa-play" />
+					</div>
+				</a>
+			</div>
+		)
+	}) 
+	
+}
 
 export default SearchModal;
